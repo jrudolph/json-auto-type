1a206a0289497a1797085949c4cc08a4c8e0aa57
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-03-02 16:11:45 +0100

    Move type inference to its own file

---
+++ src/main/scala/net/virtualvoid/jsontypes/Inferer.scala
@@ -0,0 +1,80 @@
+package net.virtualvoid.jsontypes
+
+import spray.json._
+import JsType._
+
+object Inferer {
+  def infer(value: JsValue): JsType = value match {
+    case JsString(value) => StringType(value)
+    case JsNull          => NullType
+    case _: JsNumber     => NumberType
+    case _: JsBoolean    => BooleanType
+
+    case array: JsArray =>
+      array.elements.map(infer).reduceLeftOption(unify).map(ArrayOf).getOrElse(EmptyArray)
+    case obj: JsObject =>
+      ObjectOf(obj.fields.mapValues(infer))
+  }
+  def unify(one: JsType, two: JsType): JsType = {
+    def addToOneOf(existing: Alternatives, newEntry: JsType): Alternatives = {
+      def tryOne(remaining: Seq[JsType], tried: Seq[JsType]): Alternatives =
+        remaining match {
+          case Nil =>
+            existing + newEntry // cannot be unified into any of the existing alternatives
+          case next +: rem =>
+            unify(next, newEntry) match {
+              case _: OneOf =>
+                // didn't work, got alternative
+                tryOne(rem, tried :+ next)
+              case x =>
+                // did work! use the new entry
+                tried.toSet ++ rem.toSet + x
+            }
+        }
+
+      tryOne(existing.toVector, Nil)
+    }
+    def ordering(structure: JsType): Int = structure match {
+      case NullType       => 0
+      case _: ValueOrNull => 5
+      case _: OneOf       => 10
+      case _: ArrayOf     => 20
+      case _: ObjectOf    => 30
+      case _: StringType  => 40
+      case NumberType     => 50
+      case BooleanType    => 60
+      case Missing        => 70
+      case EmptyArray     => 80
+    }
+
+    if (one == two) one
+    else {
+      // as unification is commutative, ensure we only have to look for one permutation
+      val (fst, snd) =
+        if (ordering(one) < ordering(two)) (one, two)
+        else (two, one)
+
+      (fst, snd) match {
+        case (NullType, NullType) => NullType
+        case (NullType, other: ValueOrNull) => other
+        case (von @ ValueOrNull(v), v2) if unify(v, v2).isInstanceOf[PrimitiveType] => von
+        case (NullType, other) => ValueOrNull(other)
+        case (_: StringType, _: StringType) => StringType("<several>")
+        case (other: ArrayOf, EmptyArray) => other
+        case (ArrayOf(s1), ArrayOf(s2)) => ArrayOf(unify(s1, s2))
+        case (ObjectOf(fields1), ObjectOf(fields2)) =>
+          val allFields = fields1.keySet ++ fields2.keySet
+
+          val newStruct =
+            allFields.map { f =>
+              f -> unify(fields1.getOrElse(f, Missing), fields2.getOrElse(f, Missing))
+            }.toMap
+          ObjectOf(newStruct)
+
+        case (OneOf(many), OneOf(others)) => OneOf(others.foldLeft(many)(addToOneOf))
+        case (OneOf(many), x)             => OneOf(addToOneOf(many, x))
+        case (a, b)                       => OneOf(Set(a, b))
+      }
+    }
+  }
+}
